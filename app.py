202a42ef6dbb24ce033c30f4fa1cd6a64299b713
climsneyes <climsneyes85@gmail.com>
2025-06-16 23:44:24 +0900

    Cloudtype 배포를 위한 포트 설정 등 변경사항 반영

--- app.py
+++ app.py
@@ -790,4 +790,6 @@ def is_valid_law_name(name):
     return True
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    import os
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port) 
